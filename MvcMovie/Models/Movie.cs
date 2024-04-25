7f97fde32aa598951539a60c8166b6adcab8e313
Loc NGUYEN <nguyentheloc@gmail.com>
2024-04-25 13:58:44 +0700

    Add project files.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    [DisplayName("Ngày phát hành")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    public decimal Price { get; set; }
+}
